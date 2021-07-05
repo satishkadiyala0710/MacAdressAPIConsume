d95ee5682e3998edeacea71ba12ead74ad932372
bpattan <bajeed.khan@gmail.com>
2021-07-05 20:50:16 +0530

    Added try and exception block and comments

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import json
-
 import requests
 
-
-# query=input("enter company name")
-
-query="44:38:39:ff:ef:57"
-response=requests.get("https://api.macaddress.io/v1?apiKey=at_FADZ9k5dxSmsbaY7gOVI0qOxTdWJq&output=json&search={}".format(query))
+#Program for ConsumeMacAddressRestAPI
 
 
-data=(json.loads(response.content))
-
-print("Company Name:{} And  MacAdress:{}".format(data["vendorDetails"]["companyName"],data["macAddressDetails"]["searchTerm"]))
-
-
-
-
-
+try:
+    #take macaddress as input parameter
+    query=input("enter company name")
+    #fetch the company details using get method passing macadress as query parameter
+    response=requests.get("https://api.macaddress.io/v1?apiKey=at_FADZ9k5dxSmsbaY7gOVI0qOxTdWJq&output=json&search={}".format(query))
+    #parsing the response data using json loads method
+    data=(json.loads(response.content))
+    print("Company Name:{} And  MacAdress:{}".format(data["vendorDetails"]["companyName"],data["macAddressDetails"]["searchTerm"]))
+except Exception as e:
+    print(e.args)
 
 
 
 
-# Press the green button in the gutter to run the script.
+
+
+
+
+
 if __name__ == '__main__':
     pass
 
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
